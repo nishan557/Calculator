0c3df59903b818c2189d3bbe809d1f9d158b7a2c
Nishan <nkhadka912@gmail.com>
2024-07-08 22:36:17 +0545

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
-function App() {
+export default function App() {
+  let [equation, setEquation] = useState('');
+  let handleInput = (e) => {
+    setEquation(equation + e.target.value);
+  };
+
+  const handleEqual = () => {
+    try {
+      equation = eval(equation);
+      setEquation(equation);
+    } catch (error) {
+      setEquation('Error');
+    }
+  };
+
+  const handleClear = () => {
+    setEquation('');
+    setEquation('');
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="Maindiv">
+        <div>
+          <input type="text" value={equation} onChange={handleInput} />
+        </div>
+        <br />
+        <div className="subdiv">
+          <button onClick={() => handleInput({ target: { value: '0' } })}>0</button>
+          <button onClick={() => handleInput({ target: { value: '1' } })}>1</button>
+          <button onClick={() => handleInput({ target: { value: '2' } })}>2</button>
+          <button onClick={() => handleInput({ target: { value: '3' } })}>3</button>
+          <button onClick={() => handleInput({ target: { value: '4' } })}>4</button>
+          <button onClick={() => handleInput({ target: { value: '5' } })}>5</button>
+          <button onClick={() => handleInput({ target: { value: '6' } })}>6</button>
+          <button onClick={() => handleInput({ target: { value: '7' } })}>7</button>
+          <button onClick={() => handleInput({ target: { value: '8' } })}>8</button>
+          <button onClick={() => handleInput({ target: { value: '9' } })}>9</button>
+          <button onClick={() => handleInput({ target: { value: '+' } })} className='operator'>+</button>
+          <button onClick={() => handleInput({ target: { value: '-' } })} className='operator'>-</button>
+          <button onClick={() => handleInput({ target: { value: '*' } })} className='operator'>*</button>
+          <button onClick={() => handleInput({ target: { value: '/' } })} className='operator'>/</button>
+          <button onClick={() => handleInput({ target: { value: '%' } })} className='operator'>%</button>
+          <br />
+          <button className='operator' onClick={handleEqual}>=</button>
+          <button onClick={handleClear}>C</button>
+        </div>
+      </div>
     </div>
   );
 }
-
-export default App;
